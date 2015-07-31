761c343fcd6fda2d04cc0815c689a9630e12f58e
Nigel Tao <nigeltao@golang.org>
2015-07-31 16:31:30 +1000

    shiny/driver/internal/swizzle: new package.
    On my amd64 desktop machine:
    BenchmarkBGRA-8      	    3000	    469214 ns/op
    BenchmarkPureGoBGRA-8	     500	   3267103 ns/op
    
    When swizzling a 1920x1080 RGBA pixel buffer, there's a 7x difference
    between 3.27ms and 0.47ms, and that 3-ish milliseconds difference is a
    noticable fraction of the 16.67ms that a 60Hz refresh rate gives you.
    
    Thanks to Aaron Jacobs for his help with SIMD assembly.
    
    Change-Id: I8c1a50cc3f038824e07442492f8f0f6b22c83728

---
+++ shiny/driver/internal/swizzle/swizzle_doc.go
@@ -0,0 +1,26 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package swizzle provides functions for converting between RGBA pixel
+// formats.
+package swizzle
+
+// BGRA converts a pixel buffer between Go's RGBA and other systems' BGRA byte
+// orders.
+func BGRA(p []byte) {
+	if len(p)%4 != 0 {
+		return
+	}
+
+	// Use SIMD code for 16-byte chunks, if supported.
+	if haveSIMD16 {
+		n := len(p) &^ (16 - 1)
+		bgra16(p[:n])
+		p = p[n:]
+	}
+
+	for i := 0; i < len(p); i += 4 {
+		p[i+0], p[i+2] = p[i+2], p[i+0]
+	}
+}
