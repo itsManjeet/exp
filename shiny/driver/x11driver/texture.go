1ed6bc6c689a176d927c19d97e2d639aef95827a
David Crawshaw <crawshaw@golang.org>
2015-10-06 10:01:24 -0400

    shiny/driver/x11driver: honor draw.Op where possible
    When drawing onto a square region of a window we can use the clipping
    built into the X11 Composite command. As we have a complete map from src
    to dst for pixels, it's possible to support draw.Src in this case.
    
    For the case of a general affine transform, because X11 works in the
    opposite direction to us (we think in terms of src2dst, it thinks in
    terms of dst2src), there's no easy way to support draw.Src without doing
    the composition ourselves. So instead always use draw.Over in that case.
    
    Fixes example/tile
    
    Change-Id: Id199393c5dd4755bcec5fc2accb6dfb2077861a2

--- shiny/driver/x11driver/texture.go
+++ shiny/driver/x11driver/texture.go
@@ -5,10 +5,10 @@
 package x11driver
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
+	"log"
 	"sync"
 
 	"github.com/BurntSushi/xgb/render"
@@ -71,29 +71,48 @@ func inv(x *f64.Aff3) *f64.Aff3 {
 }
 
 func (t *textureImpl) draw(xp render.Picture, src2dst *f64.Aff3, sr image.Rectangle, op draw.Op, w, h int, opts *screen.DrawOptions) {
-	// TODO: honor sr.Max
+	pictOp := byte(render.PictOpOver)
+	dstX, dstY := 0, 0
+
+	if src2dst[1] == 0 && src2dst[3] == 0 {
+		// Drawing a square onto a square, we can honor the draw op.
+		pictOp = renderOp(op)
+
+		// We do the translation component of the transform by clipping
+		// the composition region in render.Composite below.
+		w, h = sr.Dx(), sr.Dy()
+		dstX = int(src2dst[2]) - sr.Min.X
+		dstY = int(src2dst[5]) - sr.Min.Y
+		src2dst[2] = 0
+		src2dst[5] = 0
+	} else {
+		// Drawing a square onto an aribtrary region of xp.
+		//
+		// As the composite extension maps from dst to src and we don't
+		// know what pixels on dst we are going to touch, we cannot honor
+		// the draw op.
+		if sr.Max != t.size {
+			// TODO: support sr.Max. We cannot do so with SetPictureClipRectangles
+			// to do it as that applies before the affine transform. We probably
+			// have to generate an intermediate picture.
+			log.Printf("x11driver.Draw: sr.Max not supported for arbitrary affine transforms")
+		}
+	}
 
 	// The XTransform matrix maps from destination pixels to source
 	// pixels, so we invert src2dst.
 	dst2src := inv(src2dst)
-	err := render.SetPictureTransformChecked(t.s.xc, t.xp, render.Transform{
+	render.SetPictureTransform(t.s.xc, t.xp, render.Transform{
 		f64ToFixed(dst2src[0]), f64ToFixed(dst2src[1]), f64ToFixed(dst2src[2]),
 		f64ToFixed(dst2src[3]), f64ToFixed(dst2src[4]), f64ToFixed(dst2src[5]),
 		f64ToFixed(0), f64ToFixed(0), f64ToFixed(1),
-	}).Check()
+	})
+	render.SetPictureFilter(t.s.xc, t.xp, uint16(len("bilinear")), "bilinear", nil)
 
-	if err != nil {
-		panic(fmt.Errorf("x11driver: cannot transform picture: %v", err))
-	}
-	err = render.SetPictureFilterChecked(t.s.xc, t.xp, uint16(len("bilinear")), "bilinear", nil).Check()
-	if err != nil {
-		panic(fmt.Errorf("x11driver: cannot filter picture: %v", err))
-	}
-
-	render.Composite(t.s.xc, renderOp(op), t.xp, 0, xp,
+	render.Composite(t.s.xc, pictOp, t.xp, 0, xp,
 		int16(sr.Min.X), int16(sr.Min.Y), // SrcX, SrcY,
 		0, 0, // MaskX, MaskY,
-		0, 0, // DstX, DstY,
+		int16(dstX), int16(dstY), // DstX, DstY,
 		uint16(w), uint16(h), // Width, Height,
 	)
 }
