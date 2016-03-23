dca05f5a83f96b6ae2ee6be636e290ffbcc14934
Burcu Dogan <jbd@google.com>
2016-03-23 11:08:54 -0700

    peripheral: inital commit of the SPI package
    Peripheral directory will include some experimental packages to
    communicate with peripheral devices. GPIO, SPI and I2C protocols are
    what I am planning to work on.
    
    The packages will support multiple backend implementations
    (e.g. devfs, sysfs) to support multiple platforms.
    
    The SPI package currently uses only the devfs driver that runs on
    Linux. Upcoming CLs will allow users to load others and distribute
    their own backends.
    
    Change-Id: I53e45a76f6d740b2356112e69a1527e964f29f7c

---
+++ peripheral/spi/example_test.go
@@ -0,0 +1,40 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package spi_test
+
+import "golang.org/x/exp/peripheral/spi"
+
+// Example illustrates a program that drives an APA-102 LED strip.
+func Example() {
+	dev, err := spi.Open("/dev/spidev0.1")
+	if err != nil {
+		panic(err)
+	}
+	defer dev.Close()
+
+	if err := dev.SetMode(spi.Mode3); err != nil {
+		panic(err)
+	}
+	if err := dev.SetSpeed(500000); err != nil {
+		panic(err)
+	}
+	if err := dev.SetBitsPerWord(8); err != nil {
+		panic(err)
+	}
+	if err := dev.Do([]byte{
+		0, 0, 0, 0,
+		0xff, 200, 0, 200,
+		0xff, 200, 0, 200,
+		0xe0, 200, 0, 200,
+		0xff, 200, 0, 200,
+		0xff, 8, 50, 0,
+		0xff, 200, 0, 0,
+		0xff, 0, 0, 0,
+		0xff, 200, 0, 200,
+		0xff, 0xff, 0xff, 0xff,
+	}, 0); err != nil {
+		panic(err)
+	}
+}
