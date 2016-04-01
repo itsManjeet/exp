4f60560ad6cf4d0415d1c15346c8c4eb77ea18e8
Nigel Tao <nigeltao@golang.org>
2016-04-01 13:21:56 +1100

    shiny/widget: add Palette and Theme types.
    The exact colors used for the default palette is arbitrary, but these
    Material Design colors seem as good a choice as any other for now.
    
    A Palette's fields roughly corresponds to what a Java Swing UI looks
    like. It might change later to accomodate something more modern, but it
    will do for now in order to make progress with upcoming widget code.
    
    Change-Id: Ibd6452e91ae281244adcaf9c91d8fbef023307b9

--- shiny/widget/widget.go
+++ shiny/widget/widget.go
@@ -9,6 +9,10 @@ package widget // import "golang.org/x/exp/shiny/widget"
 
 import (
 	"image"
+	"image/color"
+
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
 )
 
 // Arity describes the number of children a class of nodes can have.
@@ -20,6 +24,71 @@ const (
 	Container = Arity(2) // Container nodes can have any number of children.
 )
 
+// Palette is a set of colors for a theme.
+//
+// The colors are expressed as *image.Uniform values so that they can be easily
+// passed as the src argument to image/draw functions.
+type Palette struct {
+	// Light, Neutral and Dark are three color tones used to fill in widgets
+	// such as buttons, menu bars and panels.
+	Light   *image.Uniform
+	Neutral *image.Uniform
+	Dark    *image.Uniform
+
+	// Accent is the color used to accentuate selections or suggestions.
+	Accent *image.Uniform
+
+	// Foreground is the color used for text, dividers and icons.
+	Foreground *image.Uniform
+
+	// Background is the color used behind large blocks of text. Short,
+	// non-editable label text will typically be on the Neutral color.
+	Background *image.Uniform
+}
+
+// Theme is a set of colors and font faces.
+type Theme interface {
+	// Palette returns the color palette for this theme.
+	Palette() Palette
+
+	// GetFace returns a font.Face for this theme. PutFace should be called
+	// once a widget's measure, layout or paint is done with the font.Face
+	// returned.
+	//
+	// Note that, in general, a font.Face is not safe for concurrent use by
+	// multiple goroutines, as its methods may re-use implementation-specific
+	// caches and mask image buffers.
+	GetFace() font.Face
+	PutFace(font.Face)
+
+	// TODO: be able to ask for font face variants, such as bold or embiggened
+	// versions for headings vs body text?
+}
+
+var (
+	// DefaultPalette is the default theme's palette.
+	DefaultPalette = Palette{
+		Light:      &image.Uniform{C: color.RGBA{0xf5, 0xf5, 0xf5, 0xff}}, // Material Design "Grey 100".
+		Neutral:    &image.Uniform{C: color.RGBA{0xee, 0xee, 0xee, 0xff}}, // Material Design "Grey 200".
+		Dark:       &image.Uniform{C: color.RGBA{0xe0, 0xe0, 0xe0, 0xff}}, // Material Design "Grey 300".
+		Accent:     &image.Uniform{C: color.RGBA{0x21, 0x96, 0xf3, 0xff}}, // Material Design "Blue 500".
+		Foreground: &image.Uniform{C: color.RGBA{0x00, 0x00, 0x00, 0xff}}, // Material Design "Black".
+		Background: &image.Uniform{C: color.RGBA{0xff, 0xff, 0xff, 0xff}}, // Material Design "White".
+	}
+
+	// DefaultTheme is a theme using the default palette and a basic font face.
+	DefaultTheme Theme = defaultTheme{}
+)
+
+// Note that a basicfont.Face is stateless and safe to use concurrently, so
+// defaultTheme.PutFace can be a no-op.
+
+type defaultTheme struct{}
+
+func (defaultTheme) Palette() Palette   { return DefaultPalette }
+func (defaultTheme) GetFace() font.Face { return basicfont.Face7x13 }
+func (defaultTheme) PutFace(font.Face)  {}
+
 // Class is a class of nodes. For example, all button widgets would be Nodes
 // whose Class values are a ButtonClass.
 type Class interface {
@@ -28,21 +97,21 @@ type Class interface {
 
 	// Measure returns the natural size of a specific node (and its children)
 	// of this class.
-	Measure(n *Node) (size image.Point)
+	Measure(n *Node, t *Theme) (size image.Point)
 
 	// Layout lays out a specific node (and its children) of this class.
 	//
 	// TODO: specify how previous measurements and size constraints are passed
 	// down the tree.
-	Layout(n *Node)
+	Layout(n *Node, t *Theme)
 
 	// Paint paints a specific node (and its children) of this class onto a
 	// destination image.
 	//
 	// TODO: specify how previous layout is passed down the tree.
-	Paint(n *Node, dst *image.RGBA)
+	Paint(n *Node, t *Theme, dst *image.RGBA)
 
-	// TODO: add color/font themes to Measure/Layout/Paint.
+	// TODO: add DPI to Measure/Layout/Paint, via the Theme or otherwise.
 	// TODO: OnXxxEvent methods.
 }
 
@@ -51,10 +120,10 @@ type Class interface {
 // the Class interface's methods.
 type LeafClassEmbed struct{}
 
-func (LeafClassEmbed) Arity() Arity              { return Leaf }
-func (LeafClassEmbed) Measure(*Node) image.Point { return image.Point{} }
-func (LeafClassEmbed) Layout(*Node)              {}
-func (LeafClassEmbed) Paint(*Node, *image.RGBA)  {}
+func (LeafClassEmbed) Arity() Arity                      { return Leaf }
+func (LeafClassEmbed) Measure(*Node, *Theme) image.Point { return image.Point{} }
+func (LeafClassEmbed) Layout(*Node, *Theme)              {}
+func (LeafClassEmbed) Paint(*Node, *Theme, *image.RGBA)  {}
 
 // TODO: ShellClassEmbed, ContainerClassEmbed.
 
