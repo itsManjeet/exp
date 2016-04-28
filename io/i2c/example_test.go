0a2b5ae53524003e526b9ec2656dce176ba9b840
Jaana Burcu Dogan <jbd@google.com>
2016-04-28 16:39:47 +0200

    io/i2c: add an example for Open
    Change-Id: Ie9ad7a09b91f53372107dae19cb01dfd09978a94

---
+++ io/i2c/example_test.go
@@ -0,0 +1,16 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package i2c_test
+
+import (
+	"golang.org/x/exp/i2c"
+)
+
+func Example_open() {
+	d, err := i2c.Open(&i2c.Devfs{}, 1, i2c.NewAddr(0x39))
+	if err != nil {
+		panic(err)
+	}
+}
