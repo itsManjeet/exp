b6e69fefe85178d225660ff656b2a6d8fe01c0d8
David Crawshaw <crawshaw@golang.org>
2016-05-11 12:13:40 -0400

    shiny/widget/flex: add HTML printing of tests
    The small piece of JavaScript prints the test result table so that it
    can be copied directly into flex_test.go.
    
    Change-Id: I8c6cb7f07b2f40b7028cf87c42db77b6924627ff

--- shiny/widget/flex/flex_test.go
+++ shiny/widget/flex/flex_test.go
@@ -26,6 +26,101 @@ type layoutTest struct {
 	want         []image.Rectangle // final Rect of child elements
 }
 
+func (test *layoutTest) html() string {
+	buf := new(bytes.Buffer)
+	fmt.Fprintf(buf, `<style>
+#container {
+	display: flex;
+	width:   %dpx;
+	height:  %dpx;
+`, test.size.X, test.size.Y)
+
+	switch test.direction {
+	case Row:
+	case RowReverse:
+		fmt.Fprintf(buf, "\tflex-direction: row-reverse;\n")
+	case Column:
+		fmt.Fprintf(buf, "\tflex-direction: column;\n")
+	case ColumnReverse:
+		fmt.Fprintf(buf, "\tflex-direction: column-reverse;\n")
+	}
+	switch test.wrap {
+	case NoWrap:
+	case Wrap:
+		fmt.Fprintf(buf, "\tflex-wrap: wrap;\n")
+	case WrapReverse:
+		fmt.Fprintf(buf, "\tflex-wrap: wrap-reverse;\n")
+	}
+	switch test.alignContent {
+	case AlignContentStart:
+	case AlignContentEnd:
+		fmt.Fprintf(buf, "\talign-content: flex-end;\n")
+	case AlignContentCenter:
+		fmt.Fprintf(buf, "\talign-content: center;\n")
+	case AlignContentSpaceBetween:
+		fmt.Fprintf(buf, "\talign-content: space-between;\n")
+	case AlignContentSpaceAround:
+		fmt.Fprintf(buf, "\talign-content: space-around;\n")
+	case AlignContentStretch:
+		fmt.Fprintf(buf, "\talign-content: stretch;\n")
+	}
+	fmt.Fprintf(buf, "}\n")
+
+	for i, m := range test.measured {
+		fmt.Fprintf(buf, `#child%d {
+	width: %.2fpx;
+	height: %.2fpx;
+`, i, m[0], m[1])
+		c := tileColors[i]
+		fmt.Fprintf(buf, "\tbackground-color: rgb(%d, %d, %d);\n", c.R, c.G, c.B)
+		if test.layoutData != nil {
+			d := test.layoutData[i]
+			if d.MinSize.X != 0 {
+				fmt.Fprintf(buf, "\tmin-width: %dpx;\n", d.MinSize.X)
+			}
+			if d.MinSize.Y != 0 {
+				fmt.Fprintf(buf, "\tmin-height: %dpx;\n", d.MinSize.Y)
+			}
+			if d.MaxSize != nil {
+				fmt.Fprintf(buf, "\tmax-width: %dpx;\n", d.MaxSize.X)
+				fmt.Fprintf(buf, "\tmax-height: %dpx;\n", d.MaxSize.Y)
+			}
+			if d.Grow != 0 {
+				fmt.Fprintf(buf, "\tflex-grow: %f;\n", d.Grow)
+			}
+			if d.Shrink != nil {
+				fmt.Fprintf(buf, "\tflex-shrink: %f;\n", *d.Shrink)
+			}
+			// TODO: Basis, Align, BreakAfter
+		}
+		fmt.Fprintf(buf, "}\n")
+	}
+	fmt.Fprintf(buf, `</style>
+<div id="container">
+`)
+	for i := range test.measured {
+		fmt.Fprintf(buf, "\t<div id=\"child%d\"></div>\n", i)
+	}
+	fmt.Fprintf(buf, `</div>
+<pre id="out"></pre>
+<script>
+var out = document.getElementById("out");
+var container = document.getElementById("container");
+for (var i = 0; i < container.children.length; i++) {
+	var c = container.children[i];
+	var ctop = c.offsetTop - container.offsetTop;
+	var cleft = c.offsetLeft - container.offsetLeft;
+	var cbottom = ctop + c.offsetHeight;
+	var cright = cleft + c.offsetWidth;
+
+	out.innerHTML += "\t{size(" + cleft + ", " + ctop + "), size(" + cright + ", " + cbottom + ")},\n";
+}
+</script>
+`)
+
+	return buf.String()
+}
+
 var tileColors = []color.RGBA{
 	color.RGBA{0x00, 0x7f, 0x7f, 0xff}, // Cyan
 	color.RGBA{0x7f, 0x00, 0x7f, 0xff}, // Magenta
@@ -80,8 +175,7 @@ func TestLayout(t *testing.T) {
 			}
 		}
 		if bad {
-			t.Logf("Bad testNum %d", testNum)
-			// TODO print html so we can see the correct layout
+			t.Logf("Bad testNum %d:\n%s", testNum, test.html())
 		}
 		for i, n := range children {
 			if got := n.Wrappee().Rect; got != test.want[i] {
