1c5132833bde6aa340f23cf9321c3c588932b5ac
kortschak <dan.kortschak@adelaide.edu.au>
2017-11-17 18:33:35 +1030

    rand: let pre-go1.9 users use rand
    Change-Id: I02457173061aa88ee2b92fac00a350466d7deb1b

--- rand/rng_pre19.go
+++ rand/rng_pre19.go
@@ -2,11 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build go1.9
+// +build !go1.9
 
 package rand
-
-import "math/bits"
 
 // PCGSource is an implementation of a 64-bit permuted congruential
 // generator as defined in
@@ -51,7 +49,13 @@ func (pcg *PCGSource) Uint64() uint64 {
 	pcg.multiply()
 	pcg.add()
 	// XOR high and low 64 bits together and rotate right by high 6 bits of state.
-	return bits.RotateLeft64(pcg.high^pcg.low, -int(pcg.high>>58))
+	return rotateLeft(pcg.high^pcg.low, -int(pcg.high>>58))
+}
+
+func rotateLeft(x uint64, k int) uint64 {
+	const n = 64
+	s := uint(k) & (n - 1)
+	return x<<s | x>>(n-s)
 }
 
 func (pcg *PCGSource) add() {
