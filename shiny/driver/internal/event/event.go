d810a74af519bd9d4a95e8c41e128d33a713a7c8
Bryan C. Mills <bcmills@google.com>
2018-02-14 12:01:45 -0500

    shiny/driver/internal/event: use a channel in Deque instead of a sync.Cond
    This eliminates spurious wakeups in NextEvent.
    
    sync.Cond allows some other goroutine calling NextEvent to barge in
    and take the event in between the call to Signal and the corresponding
    return from Wait. In contrast, the channel transmits the event itself
    along with the wakeup signal, so if some other goroutine barges in the
    worst it can do is update the head event before the waiter can
    reacquire the mutex.
    
    In the context of Shiny itself the spurious wakeups are unlikely to
    matter much, but since Shiny is in golang.org/x it sets an example
    that others are likely to emulate.
    
    updates #21165
    
    Change-Id: I3585d89b00f532fbc955da23b9dd0e5e6aebd450

--- shiny/driver/internal/event/event.go
+++ shiny/driver/internal/event/event.go
@@ -13,28 +13,44 @@ import (
 // is usable, but a Deque value must not be copied.
 type Deque struct {
 	mu    sync.Mutex
-	cond  sync.Cond     // cond.L is lazily initialized to &Deque.mu.
-	front []interface{} // LIFO.
-	back  []interface{} // FIFO; used only when front is not empty.
+	head  chan interface{} // Buffered with length 1; empty or contains the head of the queue.
+	front []interface{}    // LIFO events after the head.
+	back  []interface{}    // FIFO events after front; used only when front is non-empty.
 }
 
 func (q *Deque) lockAndInit() {
 	q.mu.Lock()
-	if q.cond.L == nil {
-		q.cond.L = &q.mu
+	if q.head == nil {
+		q.head = make(chan interface{}, 1)
 	}
 }
 
 // NextEvent implements the screen.EventDeque interface.
-func (q *Deque) NextEvent() interface{} {
+func (q *Deque) NextEvent() (event interface{}) {
+	// If we could guarantee that q.head is always initialized, we could simplify
+	// this block (and avoid some contention on q.mu) as:
+	// 	event := <-q.head
+	// 	q.mu.Lock()
+	// 	defer q.mu.Unlock()
 	q.lockAndInit()
 	defer q.mu.Unlock()
+	select {
+	case event = <-q.head:
+	default:
+		q.mu.Unlock()
+		event = <-q.head
+		q.mu.Lock()
+	}
 
-	for len(q.front) == 0 {
-		q.cond.Wait()
+	if len(q.front) == 0 {
+		return event
 	}
 	i := len(q.front) - 1
-	event := q.front[i]
+	select {
+	case q.head <- q.front[i]:
+	default:
+		return event
+	}
 	q.front[i] = nil
 	q.front = q.front[:i]
 
@@ -53,12 +69,20 @@ func (q *Deque) Send(event interface{}) {
 	q.lockAndInit()
 	defer q.mu.Unlock()
 
+	// The head event must be stored in the head channel.
 	if len(q.front) == 0 && len(q.back) == 0 {
-		q.front = append(q.front, event)
-	} else {
-		q.back = append(q.back, event)
+		select {
+		case q.head <- event:
+		default:
+			// Maintain the invariant that q.back is unused when q.front is empty.
+			// That avoids unnecessary copying in the steady state and somewhat
+			// simplifies NextEvent.
+			q.front = append(q.front, event)
+		}
+		return
 	}
-	q.cond.Signal()
+
+	q.back = append(q.back, event)
 }
 
 // SendFirst implements the screen.EventDeque interface.
@@ -66,6 +90,12 @@ func (q *Deque) SendFirst(event interface{}) {
 	q.lockAndInit()
 	defer q.mu.Unlock()
 
-	q.front = append(q.front, event)
-	q.cond.Signal()
+	// Move the previous head, if any, to the front slice.
+	select {
+	case prev := <-q.head:
+		q.front = append(q.front, prev)
+	default:
+	}
+
+	q.head <- event
 }
