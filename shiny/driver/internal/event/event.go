915771bb13855b8029beeb88c25c0e5c3b66d619
Bryan C. Mills <bcmills@google.com>
2018-02-20 18:23:53 -0500

    shiny/driver/internal/event: use a channel in Deque instead of a sync.Cond
    This change is a simpler alternative to https://golang.org/cl/94138.
    It differs in that it does not attempt to avoid spurious wakeups, and
    therefore does not need to send the queue elements on the same channel
    as the wakeup signals.
    
    Since Shiny is in golang.org/x, it sets an example that others are
    likely to emulate. The pattern shown in this change adapts more easily
    to fit other Go concurrency patterns: for example, in this version of
    NextEvent it would be straightforward to select on cancellation of a
    context.Context.
    
    This change also makes the locking invariants more explicit: every
    Lock and Unlock operation appears in the code (rather than indirectly
    through sync.Cond.Signal), so it is more obvious that NextEvent
    reacquires q.mu (and must recheck the condition) after receiving the
    signal.
    
    Change-Id: If6ceba6c41706f78b6fc6c5ffb2f28faeedf3129

--- shiny/driver/internal/event/event.go
+++ shiny/driver/internal/event/event.go
@@ -12,16 +12,23 @@ import (
 // Deque is an infinitely buffered double-ended queue of events. The zero value
 // is usable, but a Deque value must not be copied.
 type Deque struct {
-	mu    sync.Mutex
-	cond  sync.Cond     // cond.L is lazily initialized to &Deque.mu.
-	front []interface{} // LIFO.
-	back  []interface{} // FIFO; used only when front is not empty.
+	mu       sync.Mutex
+	nonempty chan bool     // Buffered; sent with mu locked when the queue is not empty.
+	back     []interface{} // FIFO.
+	front    []interface{} // LIFO; used only when front is not empty.
 }
 
 func (q *Deque) lockAndInit() {
 	q.mu.Lock()
-	if q.cond.L == nil {
-		q.cond.L = &q.mu
+	if q.nonempty == nil {
+		q.nonempty = make(chan bool, 1)
+	}
+}
+
+func (q *Deque) signal() {
+	select {
+	case q.nonempty <- true:
+	default:
 	}
 }
 
@@ -31,7 +38,9 @@ func (q *Deque) NextEvent() interface{} {
 	defer q.mu.Unlock()
 
 	for len(q.front) == 0 {
-		q.cond.Wait()
+		q.mu.Unlock()
+		<-q.nonempty
+		q.mu.Lock()
 	}
 	i := len(q.front) - 1
 	event := q.front[i]
@@ -44,6 +53,9 @@ func (q *Deque) NextEvent() interface{} {
 			q.back[n-1] = nil
 		}
 		q.back = q.back[:0]
+	}
+	if len(q.front) != 0 {
+		q.signal()
 	}
 	return event
 }
@@ -58,7 +70,7 @@ func (q *Deque) Send(event interface{}) {
 	} else {
 		q.back = append(q.back, event)
 	}
-	q.cond.Signal()
+	q.signal()
 }
 
 // SendFirst implements the screen.EventDeque interface.
@@ -67,5 +79,5 @@ func (q *Deque) SendFirst(event interface{}) {
 	defer q.mu.Unlock()
 
 	q.front = append(q.front, event)
-	q.cond.Signal()
+	q.signal()
 }
