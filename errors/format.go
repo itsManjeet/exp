78367940603749e1f9a2047c9e8b92f525234bc0
Marcel van Lohuizen <mpvl@golang.org>
2018-10-04 00:28:22 +0200

    errors: formatting interfaces
    Change-Id: I4623144e43a789cc71e372e4579f2cb1fbaadc8b

---
+++ errors/format.go
@@ -0,0 +1,33 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package errors
+
+// A Formatter formats error messages.
+type Formatter interface {
+	// Format is implemented by errors to print a single error message.
+	// It should return the next error in the error chain, if any.
+	Format(p Printer) (next error)
+}
+
+// A Printer creates formatted error messages. It enforces that detailed
+// information is written last.
+//
+// Printer implementations are provided by the packages printing errors. This
+// will often be package fmt. Packages responsible for localization may provide
+// their own implementation (see for instance golang.org/x/text/message).
+type Printer interface {
+	// Print appends args to the message output.
+	Print(args ...interface{})
+
+	// Printf writes a formatted string.
+	Printf(format string, args ...interface{})
+
+	// Detail reports whether error detail is requested.
+	// After the first call to Detail, all text written to the Printer
+	// is formatted as additional detail, or ignored when
+	// detail has not been requested.
+	// If Detail returns false, the caller can avoid printing the detail at all.
+	Detail() bool
+}
