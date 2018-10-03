48b155e513b55a87cbd18fb7ae3bd042a35277d9
Marcel van Lohuizen <mpvl@golang.org>
2018-10-04 00:28:42 +0200

    errors/fmt: implementation of formatting interfaces
    Change-Id: I9599b53a9c593aa560dd9fc3bd2d2cd0912afeae

--- errors/fmt/print.go
+++ errors/fmt/print.go
@@ -563,6 +563,11 @@ func (p *pp) handleMethods(verb rune) (handled bool) {
 	if p.erroring {
 		return
 	}
+	if err, ok := p.arg.(error); ok {
+		handled = true
+		defer p.catchPanic(p.arg, verb)
+		return fmtError(p, verb, err)
+	}
 	// Is it a Formatter?
 	if formatter, ok := p.arg.(Formatter); ok {
 		handled = true
@@ -586,18 +591,7 @@ func (p *pp) handleMethods(verb rune) (handled bool) {
 		// Println etc. set verb to %v, which is "stringable".
 		switch verb {
 		case 'v', 's', 'x', 'X', 'q':
-			// Is it an error or Stringer?
-			// The duplication in the bodies is necessary:
-			// setting handled and deferring catchPanic
-			// must happen before calling the method.
-			switch v := p.arg.(type) {
-			case error:
-				handled = true
-				defer p.catchPanic(p.arg, verb)
-				p.fmtString(v.Error(), verb)
-				return
-
-			case Stringer:
+			if v, ok := p.arg.(Stringer); ok {
 				handled = true
 				defer p.catchPanic(p.arg, verb)
 				p.fmtString(v.String(), verb)
