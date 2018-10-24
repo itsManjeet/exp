495c6308574de6085150b1117a443fa90f3ac796
Marcel van Lohuizen <mpvl@golang.org>
2018-10-24 13:11:10 +0200

    errors: add stack frame API
    Fork of earlier work of dneil, with some
    significant alterations.
    
    Notes:
    by embedding Frame in the errors, the errors
    will expose the Location method. This was
    a request of several people.
    
    Frame deliberately does not implement
    errors.Formatter.
    
    Frame details includes the full function name
    and file path, as this seems to be the default
    for the most popular non-core errors package.
    Having the full file path, at least, is useful,
    as it makes it a clickable link in some shells
    and IDEs.
    If this is ever an issue, we could let fmt do
    the rendering for Frame, and use precision
    info from fmt.State to alter.
    
    Change-Id: I0dc9f9e05c86e47a140ecfe987bdcb29c145d9be

--- errors/errors.go
+++ errors/errors.go
@@ -13,13 +13,23 @@ package errors
 // errorString is a trivial implementation of error.
 type errorString struct {
 	s string
+	Frame
 }
 
 // New returns an error that formats as the given text.
+//
+// The returned error embeds a Frame set to the caller's location and implements
+// Formatter to show this information when printed with details.
 func New(text string) error {
-	return &errorString{text}
+	return &errorString{text, Caller(1)}
 }
 
 func (e *errorString) Error() string {
 	return e.s
+}
+
+func (e *errorString) Format(p Printer) (next error) {
+	p.Print(e.s)
+	e.Frame.Format(p)
+	return nil
 }
