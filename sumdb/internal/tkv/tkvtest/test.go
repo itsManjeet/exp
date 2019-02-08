d9b6155791e80cf7deabc796e757b2b70bfffe36
Russ Cox <rsc@golang.org>
2019-02-08 10:40:11 -0500

    sumdb/internal/tkv/tkvtest: add tester and in-memory implementation
    This is part of a Go checksum database implementation.
    Eventually the code will live outside golang.org/x/exp.
    
    Although the expected implementation of tkv.Storage
    is a real database or key-value store, it speeds testing
    considerably to avoid needing a full-blown storage system
    for writing tests. This CL adds tkvtest.Mem, a trivial in-memory
    implementation of tkv.Storage, for testing.
    
    This CL also adds a preliminary tester for tkv.Storage
    implementations. It is used here for testing tkvtest.Mem
    but can be used for testing other implementations as well.
    
    Change-Id: Ib70bc25a7b519fa4b685f6ce4c91b3ed04e0aa30

---
+++ sumdb/internal/tkv/tkvtest/test.go
@@ -0,0 +1,55 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package tkvtest contains a test harness for testing tkv.Storage implementations.
+package tkvtest
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"golang.org/x/exp/sumdb/internal/tkv"
+)
+
+func TestStorage(t *testing.T, ctx context.Context, storage tkv.Storage) {
+	s := storage
+
+	// Insert records.
+	err := s.ReadWrite(ctx, func(ctx context.Context, tx tkv.Transaction) error {
+		for i := 0; i < 10; i++ {
+			err := tx.BufferWrites([]tkv.Write{
+				{Key: fmt.Sprint(i), Value: fmt.Sprint(-i)},
+				{Key: fmt.Sprint(1000 + i), Value: fmt.Sprint(-1000 - i)},
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Read the records back.
+	err = s.ReadOnly(ctx, func(ctx context.Context, tx tkv.Transaction) error {
+		for i := int64(0); i < 1010; i++ {
+			if i == 10 {
+				i = 1000
+			}
+			val, err := tx.ReadValue(ctx, fmt.Sprint(i))
+			if err != nil {
+				t.Fatalf("reading %v: %v", i, err)
+			}
+			if val != fmt.Sprint(-i) {
+				t.Fatalf("ReadValue %v = %q, want %v", i, val, fmt.Sprint(-i))
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
