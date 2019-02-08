2a95a716d8fed1f0b679f6487bf2738360f6ad3d
Russ Cox <rsc@golang.org>
2019-02-08 10:49:17 -0500

    notary/internal/database/spandb: database.DB backed by Spanner
    This is part of a design sketch for a Go module notary.
    Eventually the code will live outside golang.org/x/exp.
    
    This CL adds an implementation of database.DB
    backed by a Google Cloud Spanner database.
    
    Change-Id: I08769ece5f00c133ec18fa72b215b36ad503a753

--- notary/go.mod
+++ notary/go.mod
@@ -2,4 +2,9 @@ module golang.org/x/exp/notary
 
 go 1.13
 
-require golang.org/x/crypto v0.0.0-20190313024323-a1f597ede03a
+require (
+	cloud.google.com/go v0.37.0
+	golang.org/x/crypto v0.0.0-20190313024323-a1f597ede03a
+	google.golang.org/genproto v0.0.0-20190307195333-5fe7a883aa19
+	google.golang.org/grpc v1.19.0
+)
---
+++ notary/internal/database/spandb/db_test.go
@@ -0,0 +1,124 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package spandb
+
+import (
+	"context"
+	"flag"
+	"testing"
+
+	"golang.org/x/exp/notary/internal/database"
+)
+
+var testInstance = flag.String("spanner", "", "test spanner instance (projects/xxx/instances/yyy)")
+
+func TestSpannerDB(t *testing.T) {
+	if *testInstance == "" {
+		t.Skip("no test instance given in -spanner flag")
+	}
+
+	// Test basic spanner operations
+	// (exercising interface wrapper, not spanner itself).
+	ctx := context.Background()
+
+	DeleteTestDB(ctx, *testInstance+"/databases/test_spandb")
+	db, err := CreateDB(ctx, *testInstance+"/databases/test_spandb")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_ = (database.DB)(db)
+	defer DeleteTestDB(ctx, *testInstance+"/databases/test_spandb")
+
+	// Create table.
+	err = db.CreateTables(ctx, []*database.Table{
+		{
+			Name: "t1",
+			Columns: []database.Column{
+				{
+					Name: "c1",
+					Type: "int64",
+				},
+				{
+					Name: "c2",
+					Type: "int64",
+				},
+			},
+			PrimaryKey: []string{"c2"},
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Insert some records.
+	err = db.ReadWrite(ctx, func(ctx context.Context, tx database.Transaction) error {
+		for i := int64(0); i < 10; i++ {
+			err := tx.BufferWrite([]database.Mutation{
+				{
+					Table: "t1",
+					Op:    database.Insert,
+					Cols:  []string{"c1", "c2"},
+					Vals:  []interface{}{i, -i},
+				},
+				{
+					Table: "t1",
+					Op:    database.Insert,
+					Cols:  []string{"c2", "c1"},
+					Vals:  []interface{}{-1000 - i, 1000 + i},
+				},
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Read them back.
+	err = db.ReadOnly(ctx, func(ctx context.Context, tx database.Transaction) error {
+		for i := int64(0); i < 1010; i++ {
+			if i == 10 {
+				i = 1000
+			}
+			row, err := tx.ReadRow(ctx, "t1", database.Key{-i}, []string{"c1"})
+			if err != nil {
+				tx.Read(ctx, "t1", database.Keys{All: true}, []string{"c1", "c2"}).Do(func(r database.Row) error {
+					var i, j int64
+					r.Column(0, &i)
+					r.Column(1, &j)
+					t.Logf("%v\t%v", i, j)
+					return nil
+				})
+				t.Fatalf("reading %v: %v", -i, err)
+			}
+			var j int64
+			err = row.Column(0, &j)
+			if err != nil {
+				t.Fatalf("reading %v column 0: %v", -i, err)
+			}
+			if j != i {
+				t.Errorf("reading %v column 0: have %v, want %v", -i, j, i)
+			}
+		}
+
+		err := tx.Read(ctx, "t1", database.Keys{List: []database.Key{{-999}, {0}}}, []string{"c1", "c2"}).Do(func(r database.Row) error {
+			var i, j int64
+			r.Column(0, &i)
+			r.Column(1, &j)
+			t.Logf("%v\t%v", i, j)
+			return nil
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
