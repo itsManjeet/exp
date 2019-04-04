0987cf513a4292b0787b356cc31bab4dd45223eb
Jean de Klerk <deklerk@google.com>
2019-04-04 15:04:59 -0600

    modgraphviz: add modgraphviz
    modgraphviz is a very light piece of code that turns `go mod graph`
    output into a .dot output that can be visualized.
    
    Change-Id: I04e960951ddf4c1376922af942c7ac14faf0cc14

---
+++ cmd/modgraphviz/main.go
@@ -0,0 +1,35 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The modgraphviz command translates the output for `go mod graph` into .dot
+// notation, which can then be parsed by `dot` into visual graphs.
+//
+// Usage: GO111MODULE=on go mod graph | modgraphviz | dot -Tpng -o outfile.png
+package main
+
+import (
+	"bytes"
+	"flag"
+	"log"
+	"os"
+
+	"golang.org/x/exp/cmd/modgraphviz/internal"
+)
+
+func main() {
+	flag.Usage = func() {
+		log.Println("Usage: GO111MODULE=on go mod graph | modgraphviz | dot -Tpng -o outfile.png")
+	}
+	flag.Parse()
+
+	var out bytes.Buffer
+
+	if err := internal.Run(os.Stdin, &out); err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := out.WriteTo(os.Stdout); err != nil {
+		log.Fatal(err)
+	}
+}
