5a6398b8163eebfa88a5dd70d90e72c89a57008f
Dmitri Shuralyov <dmitshur@golang.org>
2019-04-08 01:09:37 -0400

    shiny/driver/mtldriver: add start of a Metal API-based driver
    The OpenGL API has been deprecated on macOS as of version 10.14.¹
    The replacement for OpenGL on macOS is the Metal API.² This change adds
    the start of a Metal API-based shiny driver. It can be activated by
    specifying the -tags=metal build tag on macOS 10.13+. For example:
    
    	go run -tags='example metal' golang.org/x/exp/shiny/example/icongallery
    
    The goal of this CL has been to create an MVP. As a result, the focus is
    on simplicity and correctness. Performance optimizations can come later.
    
    The Metal API is currently used only to present the final pixels to
    the screen. All rendering is performed on the CPU via the image/draw
    algorithms (see https://blog.golang.org/go-imagedraw-package).
    Future work is to use mtl.Buffer, mtl.Texture, etc., to do more of
    the rendering work on the GPU.
    
    From what I've observed so far, the Metal API is looking to be a great
    fit for implementing the screen.Screen interface. But we'll learn more
    when it's used to a greater extent of its full performance potential.
    
    GLFW v3.2.1 is used for window creation, receiving input events, etc.
    Package dmitri.shuralyov.com/gpu/mtl is used for Metal API access.
    Helper packages for Apple's Core Animation and AppKit APIs are copied.
    
    References:
    
    ¹ https://developer.apple.com/documentation/macos_release_notes/macos_mojave_10_14_release_notes?language=objc#3035786
    ² https://developer.apple.com/metal/
    
    Change-Id: I0e02d660b776820ca499bfe7d67e47a9866d530c

---
+++ shiny/driver/mtldriver/window.go
@@ -0,0 +1,145 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build darwin
+
+package mtldriver
+
+import (
+	"image"
+	"image/color"
+	"log"
+
+	"dmitri.shuralyov.com/gpu/mtl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+	"golang.org/x/exp/shiny/driver/internal/drawer"
+	"golang.org/x/exp/shiny/driver/internal/lifecycler"
+	"golang.org/x/exp/shiny/driver/mtldriver/internal/coreanim"
+	"golang.org/x/exp/shiny/screen"
+	"golang.org/x/image/draw"
+	"golang.org/x/image/math/f64"
+	"golang.org/x/mobile/event/paint"
+	"golang.org/x/mobile/event/size"
+)
+
+// windowImpl implements screen.Window.
+type windowImpl struct {
+	device mtl.Device
+	window *glfw.Window
+	ml     coreanim.MetalLayer
+	cq     mtl.CommandQueue
+
+	eventCh    chan interface{}
+	size       *image.Point // Set by FramebufferSizeCallback, consumed by NextEvent.
+	lifecycler lifecycler.State
+
+	rgba    *image.RGBA
+	texture mtl.Texture // Used in Publish.
+}
+
+func (w *windowImpl) Release() {
+	w.window.Destroy()
+}
+
+func (w *windowImpl) Send(event interface{}) {
+	// TODO: use internal/event.Deque?
+	w.eventCh <- event
+	glfw.PostEmptyEvent()
+}
+
+func (w *windowImpl) SendFirst(event interface{}) {
+	// TODO: use internal/event.Deque?
+	w.eventCh <- event
+	glfw.PostEmptyEvent()
+}
+
+func (w *windowImpl) NextEvent() interface{} {
+	glfw.PollEvents()
+
+	for {
+		// Consume w.size, if any.
+		if w.size != nil {
+			// Set drawable size, create backing image and texture.
+			w.ml.SetDrawableSize(w.size.X, w.size.Y)
+			w.rgba = image.NewRGBA(image.Rectangle{Max: *w.size})
+			w.texture = w.device.MakeTexture(mtl.TextureDescriptor{
+				PixelFormat: mtl.PixelFormatRGBA8UNorm,
+				Width:       w.size.X,
+				Height:      w.size.Y,
+				StorageMode: mtl.StorageModeManaged,
+			})
+
+			// Send size and paint events.
+			w.eventCh <- size.Event{
+				WidthPx:  w.size.X,
+				HeightPx: w.size.Y,
+				// TODO: ppp,
+			}
+			w.eventCh <- paint.Event{External: true}
+
+			w.size = nil
+		}
+
+		// Return existing events in eventCh, if any.
+		select {
+		case e := <-w.eventCh:
+			return e
+		default:
+		}
+
+		// Wait for more events, if eventCh was empty.
+		glfw.WaitEvents()
+	}
+}
+
+func (w *windowImpl) Publish() screen.PublishResult {
+	// Copy w.rgba pixels into a texture.
+	region := mtl.RegionMake2D(0, 0, w.texture.Width, w.texture.Height)
+	bytesPerRow := 4 * w.texture.Width
+	w.texture.ReplaceRegion(region, 0, &w.rgba.Pix[0], uintptr(bytesPerRow))
+
+	drawable, err := w.ml.NextDrawable()
+	if err != nil {
+		log.Println("Window.Publish: couldn't get the next drawable:", err)
+		return screen.PublishResult{}
+	}
+
+	cb := w.cq.MakeCommandBuffer()
+
+	// Copy the texture into the drawable.
+	bce := cb.MakeBlitCommandEncoder()
+	bce.CopyFromTexture(
+		w.texture, 0, 0, mtl.Origin{}, mtl.Size{w.texture.Width, w.texture.Height, 1},
+		drawable.Texture(), 0, 0, mtl.Origin{})
+	bce.EndEncoding()
+
+	cb.PresentDrawable(drawable)
+	cb.Commit()
+
+	return screen.PublishResult{}
+}
+
+func (w *windowImpl) Upload(dp image.Point, src screen.Buffer, sr image.Rectangle) {
+	draw.Draw(w.rgba, sr.Add(dp), src.RGBA(), sr.Min, draw.Src)
+}
+
+func (w *windowImpl) Fill(dr image.Rectangle, src color.Color, op draw.Op) {
+	draw.Draw(w.rgba, dr, &image.Uniform{src}, image.Point{}, op)
+}
+
+func (w *windowImpl) Draw(src2dst f64.Aff3, src screen.Texture, sr image.Rectangle, op draw.Op, _ *screen.DrawOptions) {
+	draw.NearestNeighbor.Transform(w.rgba, src2dst, src.(*textureImpl).rgba, sr, op, nil)
+}
+
+func (w *windowImpl) DrawUniform(src2dst f64.Aff3, src color.Color, sr image.Rectangle, op draw.Op, _ *screen.DrawOptions) {
+	draw.NearestNeighbor.Transform(w.rgba, src2dst, &image.Uniform{src}, sr, op, nil)
+}
+
+func (w *windowImpl) Copy(dp image.Point, src screen.Texture, sr image.Rectangle, op draw.Op, opts *screen.DrawOptions) {
+	drawer.Copy(w, dp, src, sr, op, opts)
+}
+
+func (w *windowImpl) Scale(dr image.Rectangle, src screen.Texture, sr image.Rectangle, op draw.Op, opts *screen.DrawOptions) {
+	drawer.Scale(w, dr, src, sr, op, opts)
+}
