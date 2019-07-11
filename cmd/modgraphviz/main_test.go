e7496f954a0db97faf48af97dfafefa0f688c55d
Jean de Klerk <deklerk@google.com>
2019-07-10 21:09:56 -0600

    modgraphviz: colour nodes based on whether they were picked by MVS or not
    Green nodes are those picked by MVS, whilst gray nodes are those not picked.
    
    Example output on the tools repo: https://user-images.githubusercontent.com/3584893/61019393-45338f80-a357-11e9-8d28-b06c41357c0f.png
    
    Change-Id: I2ff717642480270659640f098cdf509d479d3ca3

--- cmd/modgraphviz/main_test.go
+++ cmd/modgraphviz/main_test.go
@@ -6,25 +6,101 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
-func TestRun(t *testing.T) {
+func TestConvert(t *testing.T) {
 	in := bytes.NewBuffer([]byte(`
-test.com/A test.com/B@v1.2.3
-test.com/B test.com/C@v4.5.6
+test.com/A@v1.0.0 test.com/B@v1.2.3
+test.com/B@v1.0.0 test.com/C@v4.5.6
 `))
-	graph, err := convert(in)
+	graph, nodes, err := convert(in)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	want := `digraph gomodgraph {
-	"test.com/A" -> "test.com/B@v1.2.3"
-	"test.com/B" -> "test.com/C@v4.5.6"
-}
+	wantGraph := `	"test.com/A@v1.0.0" -> "test.com/B@v1.2.3"
+	"test.com/B@v1.0.0" -> "test.com/C@v4.5.6"
 `
-	if string(graph) != want {
-		t.Fatalf("\ngot: %s\nwant: %s", string(graph), want)
+	if string(graph) != wantGraph {
+		t.Fatalf("\ngot: %s\nwant: %s", string(graph), wantGraph)
+	}
+
+	wantNodes := []string{"test.com/A@v1.0.0", "test.com/B@v1.2.3", "test.com/B@v1.0.0", "test.com/C@v4.5.6"}
+	if !reflect.DeepEqual(nodes, wantNodes) {
+		t.Fatalf("\ngot: %s\nwant: %s", nodes, wantNodes)
+	}
+}
+
+func TestColorNodes(t *testing.T) {
+	for _, tc := range []struct {
+		name         string
+		in           []string
+		wantPicked   []string
+		wantUnpicked []string
+	}{
+		{
+			name:         "single node",
+			in:           []string{"foo@v0.0.1"},
+			wantPicked:   []string{"foo@v0.0.1"},
+			wantUnpicked: nil,
+		},
+		{
+			name:         "duplicate same node",
+			in:           []string{"foo@v0.0.1", "foo@v0.0.1"},
+			wantPicked:   []string{"foo@v0.0.1"},
+			wantUnpicked: nil,
+		},
+		{
+			name:         "multiple semver - same major",
+			in:           []string{"foo@v1.0.0", "foo@v1.3.7", "foo@v1.2.0", "foo@v1.0.1"},
+			wantPicked:   []string{"foo@v1.3.7"},
+			wantUnpicked: []string{"foo@v1.0.0", "foo@v1.0.1", "foo@v1.2.0"},
+		},
+		{
+			name:         "multiple semver - multiple major",
+			in:           []string{"foo@v1.0.0", "foo@v1.3.7", "foo@v2.2.0", "foo@v2.0.1", "foo@v1.1.1"},
+			wantPicked:   []string{"foo@v1.3.7", "foo@v2.2.0"},
+			wantUnpicked: []string{"foo@v1.0.0", "foo@v1.1.1", "foo@v2.0.1"},
+		},
+		{
+			name:         "semver and pseudo version",
+			in:           []string{"foo@v1.0.0", "foo@v1.3.7", "foo@v2.2.0", "foo@v2.0.1", "foo@v1.1.1", "foo@v0.0.0-20190311183353-d8887717615a"},
+			wantPicked:   []string{"foo@v1.3.7", "foo@v2.2.0"},
+			wantUnpicked: []string{"foo@v0.0.0-20190311183353-d8887717615a", "foo@v1.0.0", "foo@v1.1.1", "foo@v2.0.1"},
+		},
+		{
+			name: "multiple pseudo version",
+			in: []string{
+				"foo@v0.0.0-20190311183353-d8887717615a",
+				"foo@v0.0.0-20190227222117-0694c2d4d067",
+				"foo@v0.0.0-20190312151545-0bb0c0a6e846",
+			},
+			wantPicked: []string{"foo@v0.0.0-20190312151545-0bb0c0a6e846"},
+			wantUnpicked: []string{
+				"foo@v0.0.0-20190227222117-0694c2d4d067",
+				"foo@v0.0.0-20190311183353-d8887717615a",
+			},
+		},
+		{
+			name:         "semver and suffix",
+			in:           []string{"foo@v1.0.0", "foo@v1.3.8-rc1", "foo@v1.3.7"},
+			wantPicked:   []string{"foo@v1.3.8-rc1"},
+			wantUnpicked: []string{"foo@v1.0.0", "foo@v1.3.7"},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			gotPicked, gotUnpicked, err := colorNodes(tc.in)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(gotPicked, tc.wantPicked) {
+				t.Fatalf("picked: got %v, want %v", gotPicked, tc.wantPicked)
+			}
+			if !reflect.DeepEqual(gotUnpicked, tc.wantUnpicked) {
+				t.Fatalf("unpicked: got %v, want %v", gotUnpicked, tc.wantUnpicked)
+			}
+		})
 	}
 }
