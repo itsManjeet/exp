aecc52aefbc454ed164102f009505913ea1926c2
Jean de Klerk <deklerk@google.com>
2019-07-10 21:09:56 -0600

    modgraphviz: colour nodes based on whether they were picked by MVS or not
    Green nodes are those picked by MVS, whilst gray nodes are those not picked.
    
    Example output on the tools repo: https://user-images.githubusercontent.com/3584893/61019393-45338f80-a357-11e9-8d28-b06c41357c0f.png
    
    Change-Id: I2ff717642480270659640f098cdf509d479d3ca3

--- cmd/modgraphviz/main_test.go
+++ cmd/modgraphviz/main_test.go
@@ -6,25 +6,95 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
-func TestRun(t *testing.T) {
+func TestConvert(t *testing.T) {
 	in := bytes.NewBuffer([]byte(`
-test.com/A test.com/B@v1.2.3
-test.com/B test.com/C@v4.5.6
+test.com/A@v1.0.0 test.com/B@v1.2.3
+test.com/B@v1.0.0 test.com/C@v4.5.6
 `))
-	graph, err := convert(in)
+	graph, nodes, err := convert(in)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	want := `digraph gomodgraph {
-	"test.com/A" -> "test.com/B@v1.2.3"
-	"test.com/B" -> "test.com/C@v4.5.6"
-}
+	wantGraph := `	"test.com/A@v1.0.0" -> "test.com/B@v1.2.3"
+	"test.com/B@v1.0.0" -> "test.com/C@v4.5.6"
 `
-	if string(graph) != want {
-		t.Fatalf("\ngot: %s\nwant: %s", string(graph), want)
+	if string(graph) != wantGraph {
+		t.Fatalf("\ngot: %s\nwant: %s", string(graph), wantGraph)
+	}
+
+	wantNodes := []string{"test.com/A@v1.0.0", "test.com/B@v1.2.3", "test.com/B@v1.0.0", "test.com/C@v4.5.6"}
+	if !reflect.DeepEqual(nodes, wantNodes) {
+		t.Fatalf("\ngot: %s\nwant: %s", nodes, wantNodes)
+	}
+}
+
+func TestColourNodes(t *testing.T) {
+	for _, tc := range []struct {
+		name      string
+		in        []string
+		wantGreen []string
+		wantGray  []string
+	}{
+		{
+			name:      "single node",
+			in:        []string{"foo@v0.0.1"},
+			wantGreen: []string{"foo@v0.0.1"},
+			wantGray:  nil,
+		},
+		{
+			name:      "duplicate same node",
+			in:        []string{"foo@v0.0.1", "foo@v0.0.1"},
+			wantGreen: []string{"foo@v0.0.1"},
+			wantGray:  nil,
+		},
+		{
+			name:      "multiple semver - same major",
+			in:        []string{"foo@v1.0.0", "foo@v1.3.7", "foo@v1.2.0", "foo@v1.0.1"},
+			wantGreen: []string{"foo@v1.3.7"},
+			wantGray:  []string{"foo@v1.0.0", "foo@v1.0.1", "foo@v1.2.0"},
+		},
+		{
+			name:      "multiple semver - multiple major",
+			in:        []string{"foo@v1.0.0", "foo@v1.3.7", "foo@v2.2.0", "foo@v2.0.1", "foo@v1.1.1"},
+			wantGreen: []string{"foo@v1.3.7", "foo@v2.2.0"},
+			wantGray:  []string{"foo@v1.0.0", "foo@v1.1.1", "foo@v2.0.1"},
+		},
+		{
+			name:      "semver and pseudo version",
+			in:        []string{"foo@v1.0.0", "foo@v1.3.7", "foo@v2.2.0", "foo@v2.0.1", "foo@v1.1.1", "foo@v0.0.0-20190311183353-d8887717615a"},
+			wantGreen: []string{"foo@v1.3.7", "foo@v2.2.0"},
+			wantGray:  []string{"foo@v0.0.0-20190311183353-d8887717615a", "foo@v1.0.0", "foo@v1.1.1", "foo@v2.0.1"},
+		},
+		{
+			name: "multiple pseudo version",
+			in: []string{
+				"foo@v0.0.0-20190311183353-d8887717615a",
+				"foo@v0.0.0-20190227222117-0694c2d4d067",
+				"foo@v0.0.0-20190312151545-0bb0c0a6e846",
+			},
+			wantGreen: []string{"foo@v0.0.0-20190312151545-0bb0c0a6e846"},
+			wantGray: []string{
+				"foo@v0.0.0-20190227222117-0694c2d4d067",
+				"foo@v0.0.0-20190311183353-d8887717615a",
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			gotGreen, gotGray, err := colourNodes(tc.in)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(gotGreen, tc.wantGreen) {
+				t.Fatalf("greens: got %v, want %v", gotGreen, tc.wantGreen)
+			}
+			if !reflect.DeepEqual(gotGray, tc.wantGray) {
+				t.Fatalf("grays: got %v, want %v", gotGray, tc.wantGray)
+			}
+		})
 	}
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module golang.org/x/exp
 
-go 1.11
+go 1.12
 
 require (
 	github.com/BurntSushi/xgb v0.0.0-20160522181843-27f122750802
