f6c2c52397d2303cc6dc77df2be08132ca10c3e8
Jonathan Amsterdam <jba@google.com>
2021-04-16 14:27:06 -0400

    event/logging: event adapters for logging
    This CL has event adapters for the four most popular logging packages.
    
    Each adapter constructs an event and exports it.
    
    Sometimes an adapter must be configured with the exporter it will
    write to, because the logging API doesn't support contexts.
    
    Change-Id: Ie570ef6db5d589bb4d4a01406d3accde8f6141b9

---
+++ event/logging/internal/internal.go
@@ -0,0 +1,34 @@
+package internal
+
+import (
+	"time"
+
+	"golang.org/x/exp/event"
+	"golang.org/x/exp/event/keys"
+)
+
+var (
+	// TODO: these should be in event/keys.
+	LevelKey = keys.Int("level")
+	NameKey  = keys.String("name")
+	ErrorKey = keys.Value("error")
+)
+
+type TestHandler struct {
+	Got event.Event
+}
+
+func (h *TestHandler) Handle(ev *event.Event) {
+	h.Got = *ev
+	h.Got.Labels = make([]event.Label, len(ev.Labels))
+	copy(h.Got.Labels, ev.Labels)
+}
+
+var TestAt = time.Now()
+
+func NewTestExporter() (*event.Exporter, *TestHandler) {
+	te := &TestHandler{}
+	e := event.NewExporter(te)
+	e.Now = func() time.Time { return TestAt }
+	return e, te
+}
