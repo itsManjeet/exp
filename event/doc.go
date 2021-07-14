804c314931ca807536de6a8139fc02be08299173
Ian Cottrell <iancottrell@google.com>
2021-07-14 00:36:28 -0400

    event: improve package documentation
    Fixes golang/go#47154
    
    Change-Id: I178ae897af03a9bf284e3e8110d3b1440e5b978e

--- event/doc.go
+++ event/doc.go
@@ -2,8 +2,18 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package event provides the core functionality for observability that allows
-// libraries using it to interact well.
-// It enforces the middle layer interchange format, but allows both frontend
-// wrappers and back end exporters to customize the usage.
+// Package event provides logging tracing and metric functionality for
+// observability.
+//
+// This packages is highly experimental and in a state of flux, do not use
+// unless you are doing so to provide feedback on the design or collaborate on
+// the development.
+//
+// It uses a common event system to provide a clean way for libraries to produce
+// observability information in a way that does not tie the libraries to a
+// specific API or applications to a specific export format.
+//
+// It is designed for minimal overhead when no exporter is used so that it is
+// safe to leave calls in libraries.
+
 package event
