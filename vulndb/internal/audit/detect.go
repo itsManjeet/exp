5918841b040fea854602ec62c445572a28a518dd
Zvonimir Pavlinovic <zpavlinovic@google.com>
2021-08-02 17:56:08 -0700

    exp/vulndb/internal/audit: push back potential FPs in ordering of findings
    The majority of experiments suggest that false positive (FP) findings
    involve call stacks that go through standard libraries. This CL adjusts
    ordering of findings to deprioritize such findings when presented to
    users.
    
    Change-Id: I9e4c3bae2befa8c7fb4eb48662f74032cc7850b5

--- vulndb/internal/audit/detect.go
+++ vulndb/internal/audit/detect.go
@@ -86,9 +86,13 @@ type Finding struct {
 	Type     SymbolType
 	Trace    []TraceElem
 
-	// Approximate measure for indicating how useful the finding might be to the audit client.
-	// The smaller the weight, the more useful is the finding.
+	// Approximate measure for indicating how understandable the finding is to the client.
+	// The smaller the weight, the more understandable is the finding.
 	weight int
+
+	// Approximate measure for indicating confidence in finding being a true positive. The
+	// smaller the value, the bigger the confidence.
+	confidence int
 }
 
 // String method for findings.
