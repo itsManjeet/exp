a604fe2d331dfc00b84c2721203ac26d677aef21
Jean de Klerk <deklerk@google.com>
2021-08-12 16:12:15 -0600

    cmd/gorelease: use CreateFromVCS instead of CreateFromDir
    This will ignore gitignored files during the zip file creation, which means
    gitignored files won't be included in the analysis.
    
    Fixes golang/go#37413
    
    Change-Id: Id5df46408a48e0be53157d95333ef3c2e02765bc

--- cmd/gorelease/gorelease.go
+++ cmd/gorelease/gorelease.go
@@ -902,7 +902,7 @@ func copyModuleToTempDir(modPath, modRoot string) (dir string, err error) {
 		}
 	}()
 
-	if err := zip.CreateFromDir(zipFile, m, modRoot); err != nil {
+	if err := zip.CreateFromVCS(zipFile, m, modRoot, "HEAD", ""); err != nil {
 		var e zip.FileErrorList
 		if errors.As(err, &e) {
 			return "", e
