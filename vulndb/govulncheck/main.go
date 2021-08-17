1a26262e803d900a8f783bd16d14ebf8a5d3009e
Zvonimir Pavlinovic <zpavlinovic@google.com>
2021-08-17 16:30:49 -0700

    vulndb/internal/audit: move result string formating to govulncheck
    audit result string formatting was currently implemented as String
    method on the audit data structures. Formatting will likely need more
    fine grained control so it should be moved outside of audit. This also
    simplifies audit library interfaces.
    
    Change-Id: Ife9feb47de1cdac2145848f51c9902a3bdf62923

--- vulndb/govulncheck/main.go
+++ vulndb/govulncheck/main.go
@@ -21,13 +21,10 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"runtime"
 	"strings"
 
 	"golang.org/x/exp/vulndb/internal/audit"
-	"golang.org/x/exp/vulndb/internal/binscan"
 	"golang.org/x/tools/go/packages"
-	"golang.org/x/tools/go/ssa/ssautil"
 	"golang.org/x/vulndb/client"
 )
 
@@ -103,23 +100,23 @@ func main() {
 
 func projectToSingleFinding(r *audit.Results) *audit.Results {
 	nr := &audit.Results{
-		SearchMode:      r.SearchMode,
-		Vulnerabilities: r.Vulnerabilities,
-		VulnFindings:    make(map[string][]audit.Finding),
+		SearchMode:   r.SearchMode,
+		ModuleVulns:  r.ModuleVulns,
+		PackageVulns: r.PackageVulns,
 	}
 
-	for id, findings := range r.VulnFindings {
-		if len(findings) > 0 {
-			nr.VulnFindings[id] = []audit.Finding{findings[0]}
+	for _, vf := range r.VulnFindings {
+		if len(vf.Findings) > 0 {
+			nvf := &audit.VulnFindings{Vuln: vf.Vuln, Findings: []audit.Finding{vf.Findings[0]}}
+			nr.VulnFindings = append(nr.VulnFindings, nvf)
 		}
 	}
-
 	return nr
 }
 
 func writeOut(r *audit.Results, toJson bool) {
 	if !toJson {
-		os.Stdout.Write([]byte(r.String()))
+		os.Stdout.Write([]byte(resultsString(r)))
 		return
 	}
 
@@ -132,42 +129,6 @@ func writeOut(r *audit.Results, toJson bool) {
 	os.Stdout.Write([]byte{'\n'})
 }
 
-// extractModules collects modules in `pkgs` up to uniqueness of
-// module path and version.
-func extractModules(pkgs []*packages.Package) []*packages.Module {
-	modMap := map[string]*packages.Module{}
-	modKey := func(mod *packages.Module) string {
-		if mod.Replace != nil {
-			return fmt.Sprintf("%s@%s", mod.Replace.Path, mod.Replace.Version)
-		}
-		return fmt.Sprintf("%s@%s", mod.Path, mod.Version)
-	}
-
-	seen := map[*packages.Package]bool{}
-	var extract func(*packages.Package, map[string]*packages.Module)
-	extract = func(pkg *packages.Package, modMap map[string]*packages.Module) {
-		if pkg == nil || seen[pkg] {
-			return
-		}
-		if pkg.Module != nil {
-			modMap[modKey(pkg.Module)] = pkg.Module
-		}
-		seen[pkg] = true
-		for _, imp := range pkg.Imports {
-			extract(imp, modMap)
-		}
-	}
-	for _, pkg := range pkgs {
-		extract(pkg, modMap)
-	}
-
-	modules := []*packages.Module{}
-	for _, mod := range modMap {
-		modules = append(modules, mod)
-	}
-	return modules
-}
-
 func isFile(path string) bool {
 	s, err := os.Stat(path)
 	if err != nil {
@@ -177,25 +138,13 @@ func isFile(path string) bool {
 }
 
 func run(cfg *packages.Config, patterns []string, importsOnly bool, dbs []string) (*audit.Results, error) {
+	dbClient, err := client.NewClient(dbs, client.Options{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create database client: %s", err)
+	}
+
 	if len(patterns) == 1 && isFile(patterns[0]) {
-		modules, symbols, err := binscan.ExtractPackagesAndSymbols(patterns[0])
-		if err != nil {
-			return nil, err
-		}
-
-		dbClient, err := client.NewClient(dbs, client.Options{})
-		if err != nil {
-			return nil, fmt.Errorf("failed to create database client: %s", err)
-		}
-
-		vulns, err := audit.FetchVulnerabilities(dbClient, modules)
-		if err != nil {
-			return nil, fmt.Errorf("failed to load vulnerability dbs: %v", err)
-		}
-		vulns = vulns.Filter(runtime.GOOS, runtime.GOARCH)
-
-		results := audit.VulnerablePackageSymbols(symbols, vulns)
-		return &results, nil
+		return audit.VulnerablePackageSymbols(patterns[0], dbClient)
 	}
 
 	// Load packages.
@@ -213,43 +162,12 @@ func run(cfg *packages.Config, patterns []string, importsOnly bool, dbs []string
 		log.Printf("\t%d loaded packages\n", len(pkgs))
 	}
 
-	// Load database.
-	if *verboseFlag {
-		log.Println("loading database...")
-	}
-	dbClient, err := client.NewClient(dbs, client.Options{})
-	if err != nil {
-		return nil, fmt.Errorf("failed to create database client: %s", err)
-	}
-
-	modVulns, err := audit.FetchVulnerabilities(dbClient, extractModules(pkgs))
-	if err != nil {
-		return nil, fmt.Errorf("failed to fetch vulnerabilities: %v", err)
-	}
-	modVulns = modVulns.Filter(runtime.GOOS, runtime.GOARCH)
-	if *verboseFlag {
-		log.Printf("\t%d known vulnerabilities.\n", modVulns.Num())
-	}
-
-	// Load SSA.
-	if *verboseFlag {
-		log.Println("building ssa...")
-	}
-	prog, ssaPkgs := ssautil.AllPackages(pkgs, 0)
-	prog.Build()
-	if *verboseFlag {
-		log.Println("\tbuilt ssa")
-	}
-
 	// Compute the findings.
 	if *verboseFlag {
 		log.Println("detecting vulnerabilities...")
 	}
-	var results audit.Results
 	if importsOnly {
-		results = audit.VulnerableImports(ssaPkgs, modVulns)
-	} else {
-		results = audit.VulnerableSymbols(ssaPkgs, modVulns)
+		return audit.VulnerableImports(pkgs, dbClient)
 	}
-	return &results, nil
+	return audit.VulnerableSymbols(pkgs, dbClient)
 }
