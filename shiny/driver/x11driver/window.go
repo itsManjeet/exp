a7b621a10e4afa66853c91b84b547cc0f25bd9ee
Ian Lance Taylor <iant@golang.org>
2022-02-04 10:40:22 -0800

    shiny: add go.mod file
    The shiny package is not related to other exp packages.
    Put it in a separate module.
    
    This package uses a modified copy of github.com/BurntSushi/xgb.
    Move that out of the vendor directory, so that current go commands work.
    
    Change-Id: Ib3bad0823d2651ff90a9651e405ed7bcd9de94b6

--- shiny/driver/x11driver/window.go
+++ shiny/driver/x11driver/window.go
@@ -12,9 +12,9 @@ import (
 	"image/draw"
 	"sync"
 
-	"github.com/BurntSushi/xgb"
-	"github.com/BurntSushi/xgb/render"
-	"github.com/BurntSushi/xgb/xproto"
+	"golang.org/x/exp/shiny/xgb"
+	"golang.org/x/exp/shiny/xgb/render"
+	"golang.org/x/exp/shiny/xgb/xproto"
 
 	"golang.org/x/exp/shiny/driver/internal/drawer"
 	"golang.org/x/exp/shiny/driver/internal/event"
