d4d57dea5e6823e2cb3ed67f0b8c3e74a5577217
zhangyunhao <zhangyunhao@bytedance.com>
2022-04-27 10:02:25 +0000

    slices: add package-level documentation for less function
    Document that the less function requires strict weak ordering.
    
    For golang/go#52518
    
    Change-Id: Ia2d5f3137932b4c0fb0da4bfdf12f8d69b2672de

--- slices/slices.go
+++ slices/slices.go
@@ -5,6 +5,13 @@
 // Package slices defines various functions useful with slices of any type.
 // Unless otherwise specified, these functions all apply to the elements
 // of a slice at index 0 <= i < len(s).
+//
+// Note that the less function in IsSortedFunc, SortFunc, SortStableFunc requires a
+// strict weak ordering(https://en.wikipedia.org/wiki/Weak_ordering#Strict_weak_orderings),
+// or the sorting may fail to sort correctly. A common case is when sorting slices of
+// floating-point numbers containing Not-a-number (NaN) values. In this case, use
+// func(a, b float64) bool {return a < b || (math.IsNaN(a) && !math.IsNaN(b))}
+// can still keep the strict weak ordering.
 package slices
 
 import "golang.org/x/exp/constraints"
