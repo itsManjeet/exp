f3e61e3984b253bfdfcc31b94b8f775d56184cb0
Guo Hui <gh73962@gmail.com>
2022-05-16 13:06:21 +0000

    apidiff: ignore internal packages
    A feature is added to apidiff that allows users to ignore internal package comparisons.
    
    Fixes golang/go#52864.
    
    Change-Id: Ic390e198ef81901a9aed4212bc6f49e73ab78336
    GitHub-Last-Rev: eb63de3ce8afb054df12a695a28ab86a43d88cf0
    GitHub-Pull-Request: golang/exp#32

--- cmd/apidiff/main.go
+++ cmd/apidiff/main.go
@@ -8,6 +8,7 @@ import (
 	"go/token"
 	"go/types"
 	"os"
+	"strings"
 
 	"golang.org/x/exp/apidiff"
 	"golang.org/x/tools/go/gcexportdata"
@@ -17,6 +18,7 @@ import (
 var (
 	exportDataOutfile = flag.String("w", "", "file for export data")
 	incompatibleOnly  = flag.Bool("incompatible", false, "display only incompatible changes")
+	allowInternal     = flag.Bool("allow-internal", false, "allow apidiff to compare internal packages")
 )
 
 func main() {
@@ -44,6 +46,10 @@ func main() {
 			os.Exit(2)
 		}
 		pkg := mustLoadPackage(flag.Arg(0))
+		// if the package is an internal package, ignore it
+		if isInternalPackage(pkg.PkgPath) {
+			die("ignore internal package:%s", pkg.PkgPath)
+		}
 		if err := writeExportData(pkg, *exportDataOutfile); err != nil {
 			die("writing export data: %v", err)
 		}
@@ -54,7 +60,12 @@ func main() {
 		}
 		oldpkg := mustLoadOrRead(flag.Arg(0))
 		newpkg := mustLoadOrRead(flag.Arg(1))
-
+		if !*allowInternal {
+			if isInternalPackage(oldpkg.Path()) && isInternalPackage(newpkg.Path()) {
+				fmt.Fprintf(os.Stderr, "Ignoring internal package %s\n", oldpkg.Path())
+				os.Exit(0)
+			}
+		}
 		report := apidiff.Changes(oldpkg, newpkg)
 		var err error
 		if *incompatibleOnly {
@@ -139,4 +150,16 @@ func writeExportData(pkg *packages.Package, filename string) error {
 func die(format string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
 	os.Exit(1)
+}
+
+func isInternalPackage(pkgPath string) bool {
+	switch {
+	case strings.HasSuffix(pkgPath, "/internal"):
+		return true
+	case strings.Contains(pkgPath, "/internal/"):
+		return true
+	case pkgPath == "internal", strings.HasPrefix(pkgPath, "internal/"):
+		return true
+	}
+	return false
 }
