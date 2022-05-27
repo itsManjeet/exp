2a4bc9652ceec11eddab3460dd6fbc80dcfd3416
jeremy <jbreiding@users.noreply.github.com>
2022-05-27 01:09:49 +0000

    event: Upgrade otel sdk version
    go.opentelemetry.io/otel/metric v0.28.0 introduced some breaking changes.
    
    Taking a crack at landing those changes to allow consuming the latest Otel sdk.
    
    Change-Id: I6b05075133cde6890e6f36cd9527b415aa28f367
    GitHub-Last-Rev: d8ebdd0f731089c49792b11154aba41fbb5c0569
    GitHub-Pull-Request: golang/exp#31

--- event/otel/metric_test.go
+++ event/otel/metric_test.go
@@ -10,51 +10,76 @@ import (
 	"time"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/metric/metrictest"
-	"go.opentelemetry.io/otel/metric/number"
+	"go.opentelemetry.io/otel/sdk/metric/export/aggregation"
+	"go.opentelemetry.io/otel/sdk/metric/metrictest"
+	"go.opentelemetry.io/otel/sdk/metric/number"
 	"golang.org/x/exp/event"
 	"golang.org/x/exp/event/otel"
 )
 
 func TestMeter(t *testing.T) {
 	ctx := context.Background()
-	mp := metrictest.NewMeterProvider()
+	mp, exp := metrictest.NewTestMeterProvider()
 	mh := otel.NewMetricHandler(mp.Meter("test"))
 	ctx = event.WithExporter(ctx, event.NewExporter(mh, nil))
 	recordMetrics(ctx)
 
+	exp.Collect(ctx)
 	lib := metrictest.Library{InstrumentationName: "test"}
-	emptyLabels := map[attribute.Key]attribute.Value{}
-	got := metrictest.AsStructs(mp.MeasurementBatches)
-	want := []metrictest.Measured{
+	got := exp.Records
+	want := []metrictest.ExportRecord{
 		{
-			Name:    "golang.org/x/exp/event/otel_test/hits",
-			Number:  number.NewInt64Number(8),
-			Labels:  emptyLabels,
-			Library: lib,
+			InstrumentName:         "golang.org/x/exp/event/otel_test/hits",
+			Sum:                    number.NewInt64Number(8),
+			Attributes:             nil,
+			InstrumentationLibrary: lib,
+			AggregationKind:        aggregation.SumKind,
+			NumberKind:             number.Int64Kind,
 		},
 		{
-			Name:    "golang.org/x/exp/event/otel_test/temp",
-			Number:  number.NewFloat64Number(-100),
-			Labels:  map[attribute.Key]attribute.Value{"location": attribute.StringValue("Mare Imbrium")},
-			Library: lib,
+			InstrumentName: "golang.org/x/exp/event/otel_test/temp",
+			Sum:            number.NewFloat64Number(-100),
+			Attributes: []attribute.KeyValue{
+				{
+					Key:   attribute.Key("location"),
+					Value: attribute.StringValue("Mare Imbrium"),
+				},
+			},
+			InstrumentationLibrary: lib,
+			AggregationKind:        aggregation.SumKind,
+			NumberKind:             number.Float64Kind,
 		},
 		{
-			Name:    "golang.org/x/exp/event/otel_test/latency",
-			Number:  number.NewInt64Number(int64(1248 * time.Millisecond)),
-			Labels:  emptyLabels,
-			Library: lib,
+			InstrumentName:         "golang.org/x/exp/event/otel_test/latency",
+			Sum:                    number.NewInt64Number(int64(2503 * time.Millisecond)),
+			Count:                  2,
+			Attributes:             nil,
+			InstrumentationLibrary: lib,
+			AggregationKind:        aggregation.HistogramKind,
+			NumberKind:             number.Int64Kind,
+			Histogram: aggregation.Buckets{
+				Boundaries: []float64{5000, 10000, 25000, 50000, 100000, 250000, 500000, 1e+06, 2.5e+06, 5e+06, 1e+07},
+				Counts:     []uint64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+			},
 		},
 		{
-			Name:    "golang.org/x/exp/event/otel_test/latency",
-			Number:  number.NewInt64Number(int64(1255 * time.Millisecond)),
-			Labels:  emptyLabels,
-			Library: lib,
+			InstrumentName:         "golang.org/x/exp/event/otel_test/transmission",
+			InstrumentationLibrary: lib,
+			Sum:                    number.NewInt64Number(1234567),
+			Count:                  1,
+			AggregationKind:        aggregation.HistogramKind,
+			Histogram: aggregation.Buckets{
+				Boundaries: []float64{5000, 10000, 25000, 50000, 100000, 250000, 500000, 1e+06, 2.5e+06, 5e+06, 1e+07},
+				Counts:     []uint64{0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+			},
 		},
 	}
 
-	if diff := cmp.Diff(want, got, cmp.Comparer(valuesEqual)); diff != "" {
+	if diff := cmp.Diff(want, got, cmp.Comparer(valuesEqual), cmpopts.SortSlices(func(x, y metrictest.ExportRecord) bool {
+		return x.InstrumentName < y.InstrumentName
+	})); diff != "" {
 		t.Errorf("mismatch (-want, got):\n%s", diff)
 	}
 }
@@ -67,9 +92,11 @@ func recordMetrics(ctx context.Context) {
 	c := event.NewCounter("hits", &event.MetricOptions{Description: "Earth meteorite hits"})
 	g := event.NewFloatGauge("temp", &event.MetricOptions{Description: "moon surface temperature in Kelvin"})
 	d := event.NewDuration("latency", &event.MetricOptions{Description: "Earth-moon comms lag, milliseconds"})
+	h := event.NewIntDistribution("transmission", &event.MetricOptions{Description: "Earth-moon comms sent, bytes", Unit: event.UnitBytes})
 
 	c.Record(ctx, 8)
 	g.Record(ctx, -100, event.String("location", "Mare Imbrium"))
 	d.Record(ctx, 1248*time.Millisecond)
 	d.Record(ctx, 1255*time.Millisecond)
+	h.Record(ctx, 1234567)
 }
