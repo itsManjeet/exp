a78a7732f5ddc99378ea2693852e740086672267
Jonathan Amsterdam <jba@google.com>
2023-01-27 01:53:05 -0500

    slog: correctly represent the zero Time in a Value
    Storing the zero time.Time in a Value and then retrieving it
    gave a non-zero time. The reason is that UnixNanos is called
    to store a time.Time in a Value, and its result is undefined
    for the zero time (or any time before the year 1678 or after 2262,
    but I don't think we need to worry about those).
    
    One solution, implemented in this CL, is to use a nil *time.Location
    to represent the zero time. the time.Time.Location method never
    returns nil, so a Value with a nil *time.Location in its `any` field
    cannot be mistaken for any other Value, time.Time or otherwise.
    
    Change-Id: Icba66f2ec16e21ed574c3336e1996ed759ab1c17

--- slog/value.go
+++ slog/value.go
@@ -97,6 +97,9 @@ type timeLocation *time.Location
 // TimeValue returns a Value for a time.Time.
 // It discards the monotonic portion.
 func TimeValue(v time.Time) Value {
+	if v.IsZero() {
+		return Value{any: timeLocation(nil)}
+	}
 	return Value{num: uint64(v.UnixNano()), any: timeLocation(v.Location())}
 }
 
@@ -271,7 +274,11 @@ func (v Value) Time() time.Time {
 }
 
 func (v Value) time() time.Time {
-	return time.Unix(0, int64(v.num)).In(v.any.(timeLocation))
+	loc := v.any.(timeLocation)
+	if loc == nil {
+		return time.Time{}
+	}
+	return time.Unix(0, int64(v.num)).In(loc)
 }
 
 // LogValuer returns v's value as a LogValuer. It panics
